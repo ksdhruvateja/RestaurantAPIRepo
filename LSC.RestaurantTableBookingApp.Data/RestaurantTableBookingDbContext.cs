1687f7234f327882c5c6e9b351b471b54b20ff97
K S Dhruva Teja <96858635+ksdhruvateja@users.noreply.github.com>
2024-06-13 18:57:11 +0530

    Add files via upload

---
+++ LSC.RestaurantTableBookingApp.Data/RestaurantTableBookingDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LSC.RestaurantTableBookingApp.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace LSC.RestaurantTableBookingApp.Data
+{
+    public class RestaurantTableBookingDbContext:DbContext
+    {
+        public RestaurantTableBookingDbContext(DbContextOptions<RestaurantTableBookingDbContext> options) : base(options)
+        { }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<DiningTable> DiningTables { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<RestaurantBranch> RestaurantBranches { get; set; }
+        public DbSet<TimeSlot> TimeSlots { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+
+}
